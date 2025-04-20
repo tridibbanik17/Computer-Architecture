b4b862e5b57fa59adcf2bdd94ecc8bfed5da2f67
Tridib Banik <tridib.perfect@gmail.com>
2025-04-20 12:33:54 -0400

    LEGv8 implementation of the C statement f = g + (h - 5)

--- LEGv8/AddSub.s
+++ LEGv8/AddSub.s
@@ -1,9 +1,5 @@
 // File: AddSub.s
 // Description: LEGv8 implementation of the C statement f = g + (h - 5);
 
-        .text                   // Section containing code
-        .global _start          // Entry point (optional, for linking or simulation)
-
-_start:
-        SUBI X2, X2, #5         // X2 = h - 5
-        ADD  X0, X1, X2         // X0 = g + (h - 5)
+SUBI X2, X2, #5         // X2 = h - 5
+ADD  X0, X1, X2         // X0 = g + (h - 5)
